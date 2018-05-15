d79ec0e08969e09fed55d12ccb57af0a619b11f8
emile <emile31.roch@gmail.com>
2018-05-15 16:18:59 +0200

    Create title with description component - refs #12021

---
+++ src/web/theme/ui/molecules/TitleWithDescription/TitleWithDescription.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TitleWithDescription = ({ title, description }) => {
+  return (
+    <div className="title-with-description">
+      <div className="title-with-description__title">{title}</div>
+      <div className="title-with-description__description">{description}</div>
+    </div>
+  );
+};
+
+TitleWithDescription.PropTypes = {
+  title: PropTypes.node,
+  description: PropTypes.node
+};
+export default TitleWithDescription;
