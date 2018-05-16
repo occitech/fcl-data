3a0843dce29241224b027fbe8da16de5f77313b1
emile <emile31.roch@gmail.com>
2018-05-16 16:43:29 +0200

    Add the background image - refs #12015

--- src/web/theme/pages/Home/Home.js
+++ src/web/theme/pages/Home/Home.js
@@ -3,11 +3,22 @@ import HomeQuery from "./HomeQuery.gql";
 import EnhanceHome from "./EnhanceHome";
 import Page from "theme/ui/templates/Page";
 import { H1 } from "theme/ui/atoms/Typography/Heading";
+import createMediaUrlFromPath from "../../../utils/createMediaUrlFromPath.js";
+import "./Home.scss";
 
-const Home = ({ category, loading }) => (
-  <Page>
-    <H1>History is closer than you think!</H1>
-  </Page>
-);
+const Home = ({ category, loading }) =>
+  console.log(category) || (
+    <Page>
+      <div className="home">
+        <img
+          src={createMediaUrlFromPath(
+            "/media/catalog/category/adrien-tutin-379257-unsplash_1.png"
+          )}
+        />
+        <H1>History is</H1>
+        <H1>closer than you think!</H1>
+      </div>
+    </Page>
+  );
 
 export default EnhanceHome(HomeQuery)(Home);
