67159a13529ed44cffc55f3894b5cf9bbd46e36e
emile <emile31.roch@gmail.com>
2018-05-16 17:19:59 +0200

    Create ourselection component - refs #12016

---
+++ src/web/theme/pages/OurSelection/OurSelection.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const OurSelection = () => {
+  return <div>z</div>;
+};
+
+OurSelection.PropTypes = {};
+
+export default OurSelection;
---
+++ src/web/theme/pages/OurSelection/index.js
@@ -0,0 +1,3 @@
+import OurSelection from "./OurSelection";
+
+export default OurSelection;
