ef211aa55604457cea1442b054d9128e0ff9e0a1
emile <emile31.roch@gmail.com>
2018-05-16 10:05:58 +0200

    Add a tile list to the one page - refs #12021

--- src/web/Router.js
+++ src/web/Router.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import BrowserRouter from "react-router-dom/BrowserRouter";
 import Switch from "react-router/Switch";
 import Route from "react-router/Route";
@@ -7,18 +7,63 @@ import Layout from "./theme/ui/templates/Layout";
 import Header from "./theme/modules/Header";
 import Footer from "./theme/modules/Footer";
 import Home from "./theme/pages/Home";
-import Product from "./theme/pages/Product";
-import Checkout from "./theme/pages/Checkout";
+import TitleWithDescription from "theme/ui/molecules/TitleWithDescription";
+import ContentTile from "theme/modules/ContentTile";
 import NotFound from "./theme/pages/NotFound";
+
+const Cards = [
+  {
+    style: "light-blue",
+    title: (
+      <TitleWithDescription
+        title={<div>title</div>}
+        description={<div className="text">1st part</div>}
+      />
+    ),
+    content: <div>content</div>
+  },
+  {
+    style: "blue",
+    title: (
+      <TitleWithDescription
+        title={<div>title</div>}
+        description={<div className="text">2nd part</div>}
+      />
+    ),
+    content: <div>content</div>
+  },
+  {
+    style: "light-blue",
+    title: (
+      <TitleWithDescription
+        title={<div>title</div>}
+        description={<div className="text">3rd part</div>}
+      />
+    ),
+    content: <div>content</div>
+  }
+];
 
 export default () => (
   <BrowserRouter>
     <Layout header={<Header />} footer={<Footer />}>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/product/:sku" component={Product} />
-        <Route path="/checkout" component={Checkout} />
-
+        <Route
+          exact
+          path="/"
+          component={() => {
+            return (
+              <Fragment>
+                <Home />
+                {Cards.map(card => (
+                  <ContentTile style={card.style} title={card.title}>
+                    {card.content}
+                  </ContentTile>
+                ))}
+              </Fragment>
+            );
+          }}
+        />
         <Route component={NotFound} />
       </Switch>
     </Layout>
