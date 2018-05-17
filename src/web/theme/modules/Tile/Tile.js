6b98c0e92a25aa8621c00f7b619486a363defdac
emile <emile31.roch@gmail.com>
2018-05-17 16:04:02 +0200

    Refactor Tile to make it great again - refs #12016

--- src/web/theme/modules/Tile/Tile.js
+++ src/web/theme/modules/Tile/Tile.js
@@ -1,22 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
-import ContentTile from "./ContentTile";
 import "./Tile.scss";
 
 const Tile = ({ title, children, style, contentStyle }) => {
-  const TileClass = `tile${style ? "--" + style : ""}`;
+  const TileClass = `tile${style ? " tile--" + style : ""}`;
+  const contentClass = `tile__content${
+    contentStyle ? "--" + contentStyle : ""
+  }`;
 
   return (
     <div className={TileClass}>
-      <ContentTile style={contentStyle} title={title}>
-        {children}
-      </ContentTile>
+      <div className={contentClass}>
+        {title}
+        <div className="tile__content__description">{children}</div>
+      </div>
     </div>
   );
 };
 
 Tile.PropTypes = {
-  style: PropTypes.oneOf(["light-blue", "red", "blue"])
+  title: PropTypes.node,
+  content: PropTypes.node,
+  style: PropTypes.oneOf(["light-blue", "red", "blue"]),
+  contentStyle: PropTypes.oneOf(["default", "card"])
 };
 
 export default Tile;
