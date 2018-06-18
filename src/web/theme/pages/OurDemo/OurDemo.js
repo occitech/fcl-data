ee13ea1716666fec07708097350a33e2e5a7098f
emile <emile31.roch@gmail.com>
2018-06-18 14:34:30 +0200

    Init OurDemo component - refs #12018

---
+++ src/web/theme/pages/OurDemo/OurDemo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const OurDemo = () => {
+  return <div>content</div>;
+};
+
+OurDemo.prototype = {};
+
+export default OurDemo;
---
+++ src/web/theme/pages/OurDemo/index.js
@@ -0,0 +1,3 @@
+import OurDemo from "./OurDemo";
+
+export default OurDemo;
