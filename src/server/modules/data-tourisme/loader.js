5468dd1676341b8f74b3c87f7a4afb5a508d9b97
emile <emile31.roch@gmail.com>
2018-06-18 11:17:45 +0200

    Remove console.log

--- src/server/modules/data-tourisme/loader.js
+++ src/server/modules/data-tourisme/loader.js
@@ -8,8 +8,5 @@ export default {
       .get(
         `https://diffuseur.datatourisme.gouv.fr/webservice/1532da3f576cd29193bb2d309f339e39/${app_key}`
       )
-      .then(response => {
-        console.log("app_key", app_key);
-        return response.data.results[0];
-      })
+      .then(response => response.data.results[0])
 };
