8c211f6d3b53dff35491be4dd55a4e15a900e1a3
emile <emile31.roch@gmail.com>
2018-06-21 16:29:37 +0200

    Handle site page right part - refs #12167

--- src/web/theme/modules/Tile/Tile.js
+++ src/web/theme/modules/Tile/Tile.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Tile.scss";
 
-const Tile = ({ title, children, style, contentStyle }) => {
+const Tile = ({ title, children, style, contentStyle, rightSide }) => {
   const TileClass = `tile${style ? " tile--" + style : ""}`;
   const contentClass = `tile__content ${
     contentStyle ? "tile__content--" + contentStyle : ""
@@ -11,7 +11,10 @@ const Tile = ({ title, children, style, contentStyle }) => {
   return (
     <div className={TileClass}>
       <div className={contentClass}>
-        {title}
+        <div className="tile__content__title">{title}</div>
+        {rightSide && (
+          <div className="tile__content__right-side-data">{rightSide}</div>
+        )}
         <div className="tile__content__description">{children}</div>
       </div>
     </div>
