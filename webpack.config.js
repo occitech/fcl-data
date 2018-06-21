b13c0c5d69148f71b39f4d42650b45d24632ba79
emile <emile31.roch@gmail.com>
2018-06-21 16:36:56 +0200

    Configurate webpack to work with leaflet - refs #12167

--- webpack.config.js
+++ webpack.config.js
@@ -42,8 +42,13 @@ const universalRules = [
     loader: "graphql-tag/loader"
   },
   {
-    test: /\.scss$/,
-    include: [srcPath],
+    test: /\.s?css$/,
+    include: [
+      srcPath,
+      path.resolve(__dirname, "node_modules/leaflet"),
+      path.resolve(__dirname, "node_modules/react-dates/lib/css")
+    ],
+
     use: [
       {
         loader: "style-loader"
